f66898185be9f884b828e88007ea4f6f08ef2e54
jvalmeida1810 <joaovitoralmeida1100@hotmail.com>
2025-12-04 10:07:30 -0300

    Meu primeiro app

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  void decrement() {
+    print('decrement');
+  }
+
+  void increment() {
+    print('incrememt');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Pode entrar',
+            style: TextStyle(fontSize: 30, color: Colors.white),
+          ),
+          const Text('0', style: TextStyle(fontSize: 100, color: Colors.white)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: decrement,
+                child: Text(
+                  'Saiu',
+                  style: TextStyle(color: Colors.black
+                  , fontSize: 16),
+                ),
+              ),
+              TextButton(
+                onPressed: increment,
+                child: Text(
+                  'Entrou',
+                  style: TextStyle(color: Colors.black
+                  , fontSize: 16),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
