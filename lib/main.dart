058d294fc4918397a571ab88528c2872792e7172
jvalmeida1810 <joaovitoralmeida1100@hotmail.com>
2025-12-04 17:20:08 -0300

    interface concluida

--- lib/main.dart
+++ lib/main.dart
@@ -9,48 +9,89 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: HomePage());
+    return const MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int count = 0;
   void decrement() {
-    print('decrement');
+    
+    setState(() {
+      count--;  
+    });
+    
+    print(count);
   }
 
   void increment() {
-    print('incrememt');
+    setState(() {
+      count++;  
+    });
+    
+    print(count);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.red,
-      body: Column(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(image: AssetImage('assets/image/image.jpg'),
+          fit: BoxFit.cover,
+
+          ),
+        ), 
+        child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           const Text(
-            'Pode entrar',
-            style: TextStyle(fontSize: 30, color: Colors.white),
+            'PODE ENTRAR!',
+            style: TextStyle(fontSize: 30, color: Colors.black),
           ),
-          const Text('0', style: TextStyle(fontSize: 100, color: Colors.white)),
+          const SizedBox(height: 32,),
+          
+          Text('$count', style: const TextStyle(fontSize: 100, color: Colors.white)),
+          const SizedBox(height: 32,),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  fixedSize: const Size(100, 100),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                ),
                 onPressed: decrement,
                 child: Text(
-                  'Saiu',
+                  'SAIU',
                   style: TextStyle(color: Colors.black
                   , fontSize: 16),
                 ),
               ),
+              const SizedBox(width: 32,),
+
               TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  fixedSize: Size(100, 100),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                ),
                 onPressed: increment,
                 child: Text(
-                  'Entrou',
+                  'ENTROU',
                   style: TextStyle(color: Colors.black
                   , fontSize: 16),
                 ),
@@ -58,6 +99,7 @@ class HomePage extends StatelessWidget {
             ],
           ),
         ],
+      ),
       ),
     );
   }
